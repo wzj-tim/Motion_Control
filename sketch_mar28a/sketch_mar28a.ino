faa4f80655cfa2db4bbf47a7c7082a1456328316
wzj-tim <81309666+wzj-tim@users.noreply.github.com>
2023-04-23 22:06:53 +0800

    FreqTest

--- sketch_mar28a/sketch_mar28a.ino
+++ sketch_mar28a/sketch_mar28a.ino
@@ -2,13 +2,24 @@
 #include <Wire.h>
 #include <U8g2lib.h>
 
+#define PI 3.14159265f
+
 #define I2C_DEV_ADDR 0x78
 #define RGB_BUILTIN 2
 #define CW_pin 17
 #define CCW_pin 16
-#define pwm_out 15
+#define PWM_Channel 0
+#define PWM_Bits 10
+#define PWM_Out 15
 #define phaseA_pin 26
 #define phaseB_pin 27
+
+typedef struct{
+  uint16_t frequency;
+  float speed;
+} FreqRecord;
+
+FreqRecord freqRecord[400];
 
 
 unsigned long recordedMillis, currentMillis;
@@ -22,10 +33,16 @@ uint8_t outFlag = 0;
 uint8_t phaseA = 0;
 uint8_t phaseB = 0;
 int32_t EncoderTotal = 0;
+int32_t lastEncoder = 0;
 float currentAngle = 0;
+float currentSpeed = 0;
 
 //OLED test
 uint8_t m = 24;
+
+//Test output memort
+uint8_t mem[200];
+uint8_t memIdx = 0;
 
 U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   
 // the setup function runs once when you press reset or power the board
@@ -41,11 +58,14 @@ void setup() {
 
   recordedMillis = currentMillis = millis();//Init timers
 
-  ledcSetup(0,freq,8);//PWM setup
-  ledcAttachPin(pwm_out,0);
+  ledcSetup(PWM_Channel,freq,PWM_Bits);//PWM setup
+  ledcAttachPin(PWM_Out,PWM_Channel);
 
   attachInterrupt(digitalPinToInterrupt(phaseA_pin), InterruptA, CHANGE);
   attachInterrupt(digitalPinToInterrupt(phaseB_pin), InterruptB, CHANGE);
+
+  ledcChangeFrequency(PWM_Channel,5,PWM_Bits);
+  ledcWrite(PWM_Channel,800);
 
 }
 
@@ -78,33 +98,74 @@ void SetCounterClockwise(){
   digitalWrite(CW_pin, LOW);
 }
 
+void FreqTest(){
+  static uint16_t testFreq = 5;
+  static uint16_t testIndex = 0;
+  static uint16_t freqTime = 5000;
+  static float testSpeed = 0;
+
+  if(freqTime > 0){
+    freqTime--;
+    // Serial.printf("%d\n",freqTime);
+    testSpeed = 0.95f*testSpeed + 0.05f*currentSpeed;
+  }
+  else{
+    freqRecord[testIndex].frequency = testFreq;
+    freqRecord[testIndex].speed = testSpeed;
+    testIndex++;
+    testSpeed = 0;
+    testFreq+=5;
+    freqTime=200;
+    Serial.printf("%d\n",testFreq);
+    ledcChangeFrequency(PWM_Channel,testFreq,PWM_Bits);
+    ledcWrite(PWM_Channel,800);
+  }
+
+  if(testFreq > 1805){
+    freqTime = 200;
+    PrintMemory();
+  }
+
+
+}
+
+void PrintMemory(){
+  static uint16_t outIndex = 0;
+  if(outIndex<361){
+    Serial.printf("%d, %f \n",freqRecord[outIndex].frequency,freqRecord[outIndex].speed);
+    outIndex++;
+  }
+}
+
 // the loop function runs over and over again forever
 void loop() {
   currentMillis = millis();
 
   if(currentMillis - recordedMillis >= 1){
     msCounter += currentMillis - recordedMillis;
-    recordedMillis = currentMillis;
+    
     //execute every millisecond
+    currentAngle = EncoderTotal/99.0f*PI/180;//*360.0f/810.0f/44.0f*PI/180;//rad
+    currentSpeed = (EncoderTotal-lastEncoder)/(currentMillis - recordedMillis)/99.0f*PI/180;//rad/s
+    lastEncoder = EncoderTotal;
 
+    FreqTest();
 
+    // if(u8g2.nextPage()){
+    //   u8g2.drawBox(64-20, 32-10, 40, 20);
+    // } else{
+    //   u8g2.firstPage();
+    // }
 
-    if(u8g2.nextPage()){
-      u8g2.drawBox(64-20, 32-10, 40, 20);
-    } else{
-      u8g2.firstPage();
-    }
-
+    recordedMillis = currentMillis;
   }
   if(msCounter>=1000){
     msCounter = msCounter - 1000;
     ///don't change above
     // Serial.printf("%d \n",currentMillis);//print time
-    if(currentMillis < 4000)SetClockwise();
-    else SetCounterClockwise();
-    ledcWrite(0,200);
-    currentAngle = EncoderTotal*360.0f/810.0f/44.0f;
-    Serial.printf("%f \n",currentAngle);
+    SetClockwise();
+    // ledcWrite(0,200);
+    // Serial.printf("%f \n",currentAngle);
     // if(freq<32000)freq = freq+1000;
     // Serial.printf("%d \n",freq);
     // ledcChangeFrequency(0,freq,8);
